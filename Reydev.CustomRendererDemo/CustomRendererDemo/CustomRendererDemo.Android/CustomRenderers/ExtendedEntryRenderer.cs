a5c4aee146b53d29fd5e15f793d9093274335cb7
REY <reynald.monares.haiyan@gmail.com>
2019-07-18 17:22:43 +0800

    update

---
+++ Reydev.CustomRendererDemo/CustomRendererDemo/CustomRendererDemo.Android/CustomRenderers/ExtendedEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using CustomRendererDemo.CustomControls;
+using CustomRendererDemo.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryRenderer))]
+namespace CustomRendererDemo.Droid.CustomRenderers
+{
+    public class ExtendedEntryRenderer : EntryRenderer
+    {
+        public ExtendedEntryRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            var xfControl = Element as ExtendedEntry;
+            if (xfControl == null) return;
+
+            if(Control != null)
+            {
+                Control.SetBackgroundResource(Resource.Drawable.entrynobottomline);
+
+                var itemList = (LayerDrawable)Control.Background;
+                var bgShape = (GradientDrawable)itemList.FindDrawableByLayerId(Resource.Id.shape_id);
+                bgShape.SetStroke(xfControl.BottomlineWidth, xfControl.BottomlineColor.ToAndroid());
+            }
+        }
+    }
+}
---
+++ Reydev.CustomRendererDemo/CustomRendererDemo/CustomRendererDemo/CustomControls/ExtendedEntry.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace CustomRendererDemo.CustomControls
+{
+    public class ExtendedEntry : Entry
+    {
+        public int BottomlineWidth { get; set; } = 1;
+        public Color BottomlineColor { get; set; } = Color.Transparent;
+    }
+}
