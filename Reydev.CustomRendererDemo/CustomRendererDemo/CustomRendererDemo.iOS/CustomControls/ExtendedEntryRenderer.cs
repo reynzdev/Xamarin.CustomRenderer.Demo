19d4715b7c1ef941c9cb11146b105cd8f3fb1d34
REY <reynald.monares.haiyan@gmail.com>
2019-07-18 17:39:46 +0800

    added custom renderer ios

---
+++ Reydev.CustomRendererDemo/CustomRendererDemo/CustomRendererDemo.iOS/CustomControls/ExtendedEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using CoreAnimation;
+using CustomRendererDemo.CustomControls;
+using CustomRendererDemo.iOS.CustomControls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryRenderer))]
+namespace CustomRendererDemo.iOS.CustomControls
+{
+    public class ExtendedEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            var xfControl = Element as ExtendedEntry;
+            xfControl.SizeChanged += (ctrl, ea) =>
+            {
+                float width = 1.0f;
+
+                CALayer border = new CALayer();
+                border.BorderColor = xfControl.BottomlineColor.ToUIColor().CGColor;
+                border.Frame = new CoreGraphics.CGRect(x: 0, y: ((float)xfControl.Height - width), width: (float)xfControl.Width, height: xfControl.BottomlineWidth);
+                border.BorderWidth = width;
+
+                Control.Layer.AddSublayer(border);
+                Control.Layer.MasksToBounds = true;
+                Control.BorderStyle = UITextBorderStyle.None;
+                Control.BackgroundColor = xfControl.BackgroundColor.ToUIColor();
+            };
+        }
+    }
+}
